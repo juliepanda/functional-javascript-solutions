020a7bdac1a6b3a51933220889cfb1d0044ed607
Julie Pan <juliepan@cisco.com>
2015-06-08 15:56:12 -0700

    correct now

--- call.js
+++ call.js
@@ -1,6 +1,26 @@
-// not correct yet
+// Early solution that uses iterative method
+
+/*function duckCount() {
+  var counter = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    if(Object.prototype.hasOwnProperty.call(arguments[i], 'quack') === true) {
+      counter += 1;
+    }
+  }
+  return counter;
+}*/
+
+// Updated solution that uses recursive methods
 function duckCount() {
-  
+  return Array.prototype.slice.call(arguments).filter(
+    function(val) { // for some reasons, ternary operator doesn't work here??!?!
+      if (Object.prototype.hasOwnProperty.call(val, 'quack') === true) {
+        return true;
+      } else {
+        return false;
+      }
+    } 
+  ).length;
 }
 module.exports = duckCount;
 
